4c976c184e0824dfe90f95a6c5c81a43dd3c64a8
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-10-15 12:07:03 +0900

    feat: authSlice에 로그인 함수 구현

--- src/services/slices/authSlice.js
+++ src/services/slices/authSlice.js
@@ -1,4 +1,22 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../firebase";
+
 export const createAuthSlice = (set) => ({
   userLocalId: 0,
+  userName: "",
   setUserLocalId: (newLocalId) => set({ userLocalId: newLocalId }), 
+  setUserName: (newUserName) => set({ userName: newUserName }),
+  logIn: async () => {
+    try {
+      const response = await signInWithPopup(auth, provider);
+      const provider = new GoogleAuthProvider();
+
+      const { fullName, localId } = response["_tokenResponse"];
+
+      set({ userLocalId: localId });
+      set({ userName: fullName });
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 });
