4440693187aa69a467eb69011d11a49454f8337b
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-10-18 00:01:56 +0900

    styles: Chip 컴포넌트 css 수정

--- src/components/Chip/index.jsx
+++ src/components/Chip/index.jsx
@@ -3,8 +3,8 @@ import Container from "../UI/Container";
 
 export default function Chip({ userName }) {
   return(
-    <Container style="absolute p-5 bg-red-1 text-14 text-red-6 left-[-30px] top-2 z-toast">
-      {userName}
+    <Container style="absolute max-w-40 p-5 bg-red-1 text-14 text-red-6 left-[-35px] top-2 z-toast">
+      {userName.length > 3 ? userName.slice(0, 3) + "..." : userName}
     </Container>
   )
 }
